a8600f73785341fddd1f7b03c0decfadbbac8f00
zjhch123 <416836508@qq.com>
2020-03-21 23:54:36 +0800

    Feat: Typing

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,10 @@ export const Steps = Object.freeze({
 export const IsAndroid = navigator.userAgent.toLowerCase().split("android").length > 1;
 
 export const MediaElementId = IsAndroid ? 'i_music' : 'i_video';
+
+export const TodoContents = [
+  '学会潜水，探索海底',
+  '做一个爆裂鼓手',
+  '逛遍京城的秘密胡同',
+  '和你徒步看一场日落',
+];
--- src/pages/entrance/index.js
+++ src/pages/entrance/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { MediaController } from '../../components/media-controller';
 
 import './index.scss';
+import { Typing } from '../../components/typing';
+import { TodoContents, MediaElementId } from '../../constants';
 
 export function Entrance({
   moveForward,
 }) {
+  const [typingKey, setTypingKey] = useState(1);
+
+  useEffect(() => {
+    const addTypingKey = (e) => {
+      if (e.target.currentTime === 0) {
+        setTypingKey(typingKey + 1);
+      }
+    };
+    
+    const element = document.getElementById(MediaElementId);
+    element.addEventListener('timeupdate', addTypingKey);
+
+    return () => element.removeEventListener('timeupdate', addTypingKey);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [typingKey]);
+
   return (
     <div className="p-entrance">
       <div className="u-filter" />
-      <MediaController className="u-media-controller" />
+      <div className="m-container">
+        <MediaController className="u-media-controller" />
+        <div className="u-typing">
+          <Typing key={typingKey} contents={TodoContents} />
+        </div>
+      </div>
     </div>
   );
 }
